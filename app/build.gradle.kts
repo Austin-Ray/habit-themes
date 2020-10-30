15727f539b27cdee1ca142ccefea5180ee237206
Austin Ray <austin@austinray.io>
2020-10-30 02:19:06 -0400

    fix(room): enable incremental compilation
    Enable incremental compilation of kapt annotations for Room library.
    Gradle emitted this warning on compilation.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,11 @@ android {
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        kapt {
+            arguments {
+                arg("room.incremental", "true")
+            }
+        }
     }
     buildTypes {
         getByName("release") {
